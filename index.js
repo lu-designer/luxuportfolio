103b0b4b1758cc500babd1de633813a8bda8f33c
Lu XU <61763261+lu-designer@users.noreply.github.com>
2023-01-04 12:55:42 +1100

    Add files via upload
    new commitment

---
+++ index.js
@@ -0,0 +1,59 @@
+
+
+
+
+
+function setNewImg1() {
+  document.getElementById("projectOneAnimation").src = "images/SJET-index-mouseHover1_6.gif";
+
+}
+
+function setOldImg1() {
+  document.getElementById("projectOneAnimation").src = "images/MQ-N.png";
+
+}
+
+
+
+function setNewImg2() {
+  document.getElementById("projectTwoAnimation").src = "images/big-w1.png";
+
+}
+
+function setOldImg2() {
+  document.getElementById("projectTwoAnimation").src = "images/bigW-index-mousehover.png";
+
+}
+
+
+function setNewImg3() {
+  document.getElementById("projectThreeAnimation").src = "images/usability testing -img.png";
+
+}
+
+function setOldImg3() {
+  document.getElementById("projectThreeAnimation").src = "images/nsw electoral new.png";
+
+}
+
+
+function setNewImg4() {
+  document.getElementById("projectFourAnimation").src = "images/p4-overhead pic-1.gif";
+
+}
+
+function setOldImg4() {
+  document.getElementById("projectFourAnimation").src = "images/kongming lantern-index.png";
+
+}
+
+
+function setNewImg5() {
+  document.getElementById("projectFiveAnimation").src = "images/SJET-index-mouseHover1_1.gif";
+
+}
+
+function setOldImg5() {
+  document.getElementById("projectFiveAnimation").src = "images/belonging-indexImg.png";
+
+}
